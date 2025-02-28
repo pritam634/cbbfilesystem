790ca4d78e3e20f06ffca6c72b9f6884bb44911c
pritam634 <83156982+pritam634@users.noreply.github.com>
2025-03-01 01:16:40 +0530

    Add files

---
+++ data.js
@@ -0,0 +1,49 @@
+const dataFromJS = [
+    {
+        fileName: "Sharp Ferro Alloys Pvt Ltd [All Docs]",
+        place: "Cab - 7/1"
+    },
+    {
+        fileName: "Calcutta Power Construction [OD]",
+        place: "Cab - 3/2"
+    },
+    {
+        fileName: "Saran Alloys Pvt Ltd [All Docs]",
+        place: "Cab - 8/2"
+    },
+    {
+        fileName: "Amit Metalics [All Docs]",
+        place: "Cab - 7/2"
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// append in the table
+dataFromJS.forEach((row) => {
+    document.querySelector("#tableBody").innerHTML=`<tr>
+            <td>${row.fileName}</td>
+            <td>${row.place}</td>
+        </tr>
+        `;
+})
